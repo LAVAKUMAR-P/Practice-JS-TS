7390eed164d590a5c868f1ab146e60ffdda32e50
Lavakumar <84187714+LAVAKUMAR-P@users.noreply.github.com>
2024-07-30 07:51:18 +0530

    Array

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ var num = 1;
 var arr = [{}];
 var valid = true;
 console.log(data);
+//null & undefied Data type 
+var n = null;
+var u = undefined;
+//Array 
+var list1 = [1, 2, 3, 4, 5];
+var list2 = [1, 2, 3, 4, 5];
--- main.ts
+++ main.ts
@@ -4,3 +4,11 @@ let num: number = 1;
 let arr: Array<object> = [{}];
 let valid: boolean = true;
 console.log(data);
+
+//null & undefied Data type 
+let n:null = null;
+let u:undefined = undefined;
+
+//Array 
+let list1 : number[]= [1,2,3,4,5];
+let list2 : Array<Number>=[1,2,3,4,5];
