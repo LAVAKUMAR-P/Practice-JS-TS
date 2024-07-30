8691a1130977b354606250c372c10d1f376acb4d
Lavakumar <84187714+LAVAKUMAR-P@users.noreply.github.com>
2024-07-30 07:40:49 +0530

    typescript practice

---
+++ main.js
@@ -0,0 +1,6 @@
+var data = "hello";
+data = "data";
+var num = 1;
+var arr = [{}];
+var valid = true;
+console.log(data);
---
+++ main.ts
@@ -0,0 +1,6 @@
+let data: string = "hello";
+data = `data`;
+let num: number = 1;
+let arr: Array<object> = [{}];
+let valid: boolean = true;
+console.log(data);
