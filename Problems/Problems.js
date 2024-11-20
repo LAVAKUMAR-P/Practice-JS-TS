9a6b19f65831ef7c7fb29677ab66be1e4d6087bf
Lavakumar <84187714+LAVAKUMAR-P@users.noreply.github.com>
2024-11-20 22:26:25 +0530

    //Remove Duplicate from an Array

--- Problems/Problems.js
+++ Problems/Problems.js
@@ -4,3 +4,7 @@ let data = [1,2,3,4,4,5,5,6]
 
 let duplicate = data.filter((num,inx)=> data.indexOf(num) !== inx);
 console.log(duplicate);
+
+//Remove Duplicate from an Array 
+let withoutDuplicate = [...new Set([...data])];
+console.log(withoutDuplicate)
