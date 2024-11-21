2fc6fcfb098b7a69183614a7dc125aaaff9cd98c
Lavakumar <84187714+LAVAKUMAR-P@users.noreply.github.com>
2024-11-21 20:50:52 +0530

    polyfill for Custom filter

--- Problems/Problems.js
+++ Problems/Problems.js
@@ -3,8 +3,38 @@
 let data = [1,2,3,4,4,5,5,6]
 
 let duplicate = data.filter((num,inx)=> data.indexOf(num) !== inx);
-console.log(duplicate);
+// console.log(duplicate);
 
 //Remove Duplicate from an Array 
 let withoutDuplicate = [...new Set([...data])];
-console.log(withoutDuplicate)
+// console.log(withoutDuplicate)
+
+
+
+//EG polyfill for Custom filter
+let nums = [1,2,3,4,4,5,5,6];
+if (!Array.prototype.myfilter) {
+    Array.prototype.myfilter = function (callback, thisArg) {
+      if (this == null) {
+        throw new TypeError('Array.prototype.filter called on null or undefined');
+      }
+      if (typeof callback !== 'function') {
+        throw new TypeError(callback + ' is not a function');
+      }
+  
+      let result = [];
+  
+      for (let i = 0; i < this.length; i++) {
+        
+          if (callback.call(thisArg, this[i], i, this)) {
+            result.push(this[i]);
+          }
+        
+      }
+  
+      return result;
+    };
+  }
+
+  console.log(data.filter((num,inx)=> data.indexOf(num) !== inx))
+  console.log(data.myfilter((num,inx)=> data.indexOf(num) !== inx))
